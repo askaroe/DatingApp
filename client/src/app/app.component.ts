5f1395978fcb5927482300de524e9c4121f252c7
Askar Oralkhan <askar4ik123@gmail.com>
2024-01-10 21:54:29 +0600

    moved to angular 16 and node 18.17.0 with npm 9.6.7

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: string = 'Dating app';
+
+  title = 'Dating app';
   users: any;
+
   constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
-    this.http.get('http://localhost:5255/api/users').subscribe({  
-    next: response => this.users = response,
+    this.http.get('http://localhost:5255/api/users').subscribe({
+      next: response => this.users = response,
       error: error => console.log(error),
-      complete: () => console.log('Request has completed')
+      complete: () => console.log('Request completed')
     })
   }
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
