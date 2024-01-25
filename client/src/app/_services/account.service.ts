2430b7d91081f5df57465313696eea7865feaa81
Askar Oralkhan <askar4ik123@gmail.com>
2024-01-25 21:47:34 +0600

    chore: retrieved the list of members with bearer token(always check localhost spelling)

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -8,7 +8,6 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AccountService {
-
   baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
@@ -19,18 +18,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
-          localStorage.setItem('user', JSON.stringify(user))
-          this.currentUserSource.next(user); 
-        }
-      })
-    )
-  }
-
-  register(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
-      map(user => {
-        if(user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
@@ -38,11 +26,23 @@ export class AccountService {
     )
   }
 
-  setCurrentUser(user: User){
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(response => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
