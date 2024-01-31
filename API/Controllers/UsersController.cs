03d76184ac6b7e5df162cfb84d02d219e84f004f
Askar Oralkhan <askar4ik123@gmail.com>
2024-02-01 01:11:46 +0600

    chore: added functionality to the API to update the user info in database

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.Data;
+﻿using System.Security.Claims;
+using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -36,4 +37,20 @@ public class UsersController : BaseApiController
 
         return await _userRepository.GetMemberAsync(username);
     }
+
+    [HttpPut]
+    public async Task<ActionResult<MemberDTO>> UpdateUser(MemberUpdateDTO memberUpdateDTO)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if(user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDTO, user); 
+        
+        if(await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to Update user");
+    }
+
 }
