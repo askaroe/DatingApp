60ce138f0593f5c169ffc4a05570c5e0302f4f1c
Askar Oralkhan <askar4ik123@gmail.com>
2024-02-14 19:57:34 +0600

    chore: updated the API register method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,27 +14,28 @@ public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(DataContext context, ITokenService tokenService)
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
         _context = context;
         _tokenService = tokenService;
+        _mapper = mapper;
     }
 
     [HttpPost("register")] // POST route: api/account/register
     public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
     {
 
-        if(await UserExists(registerDTO.Username)) return BadRequest("Username is already taken");
+        if (await UserExists(registerDTO.Username)) return BadRequest("Username is already taken");
+
+        var user = _mapper.Map<AppUser>(registerDTO); // AppUser - to, registerDTO - from
 
         using var hmac = new HMACSHA512();
 
-        var user = new AppUser
-        {
-            UserName = registerDTO.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
-            PasswordSalt = hmac.Key
-        };
+        user.UserName = registerDTO.Username.ToLower();
+        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password));
+        user.PasswordSalt = hmac.Key;
 
         _context.Users.Add(user);
 
@@ -43,6 +45,7 @@ public class AccountController : BaseApiController
         {
             UserName = user.UserName,
             Token = _tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
         };
     }
 
@@ -53,28 +56,29 @@ public class AccountController : BaseApiController
             .Include(p => p.Photos)
             .SingleOrDefaultAsync(u => u.UserName == loginDTO.Username.ToLower());
 
-        if(user == null) return Unauthorized("Invalid username");
+        if (user == null) return Unauthorized("Invalid username");
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
 
         var computedHashOfTheLogin = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
 
-        for(int i = 0; i < computedHashOfTheLogin.Length; i++)
+        for (int i = 0; i < computedHashOfTheLogin.Length; i++)
         {
-            if(computedHashOfTheLogin[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            if (computedHashOfTheLogin[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
         }
 
         return new UserDTO
         {
             UserName = user.UserName,
             Token = _tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            KnownAs = user.KnownAs
         };
     }
 
     private async Task<bool> UserExists(string username)
     {
-        return await _context.Users.AnyAsync(u => u.UserName == username.ToLower());    
+        return await _context.Users.AnyAsync(u => u.UserName == username.ToLower());
     }
 
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -15,5 +15,6 @@ public class AutoMapperProfiles : Profile
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDTO>();
         CreateMap<MemberUpdateDTO, AppUser>();
+        CreateMap<RegisterDTO, AppUser>();
     }
 }
