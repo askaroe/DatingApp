23c2a93b42b3b7b43ca9bbeeac7954824a94a59d
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-22 00:56:15 +0500

    feat: added pagination for the likes

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -1,10 +1,11 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces;
 public interface ILikesRepository
 {
     Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
     Task<AppUser> GetUserWithLikes(int userId);
-    Task<IEnumerable<LikeDTO>> GetUserLikes(string predicate, int userId);
+    Task<PagedList<LikeDTO>> GetUserLikes(LikesParams likesParams);
 }
