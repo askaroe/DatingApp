b0b4662c854c18a77071a0b483cbb9c2789917ca
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-21 23:05:46 +0500

    chore: added a likes repository

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,10 @@
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces;
+public interface ILikesRepository
+{
+    Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+    Task<AppUser> GetUserWithLikes(int userId);
+    Task<IEnumerable<LikeDTO>> GetUserLikes(string predicate, int userId);
+}
