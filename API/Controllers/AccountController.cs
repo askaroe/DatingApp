90f567bcba67ecb36f20814c64f4f5f5cf9bc890
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-24 01:25:22 +0500

    chore: added roles to the JWT token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -39,10 +39,14 @@ public class AccountController : BaseApiController
 
         if(!result.Succeeded) return BadRequest(result.Errors);
 
+        var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+        if(!roleResult.Succeeded) return BadRequest(roleResult.Errors);
+
         return new UserDTO
         {
             UserName = user.UserName,
-            Token = _tokenService.CreateToken(user),
+            Token = await _tokenService.CreateToken(user),
             KnownAs = user.KnownAs,
             Gender = user.Gender
         };
@@ -64,7 +68,7 @@ public class AccountController : BaseApiController
         return new UserDTO
         {
             UserName = user.UserName,
-            Token = _tokenService.CreateToken(user),
+            Token = await _tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
             KnownAs = user.KnownAs,
             Gender = user.Gender
