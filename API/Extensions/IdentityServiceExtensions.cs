327868388ecc44f2a880389c6b4f875fb7299841
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-24 01:40:07 +0500

    feat: added policy based authorization

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -32,6 +32,11 @@ public static class IdentityServiceExtensions
             };
         });
 
+        services.AddAuthorization(options => {
+            options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+            options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+        });
+
         return services;
     }
 }
