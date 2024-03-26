62599a1fd87d4ca327eca626e05e6fd4c9a2e835
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-26 17:03:05 +0500

    chore: added signalR presence hub

--- API/Extensions/ApplicaitonServiceExtensions.cs
+++ API/Extensions/ApplicaitonServiceExtensions.cs
@@ -24,6 +24,7 @@ public static class ApplicaitonServiceExtensions
         services.AddScoped<LogUserActivity>();
         services.AddScoped<ILikesRepository, LikesRepository>();
         services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddSignalR();
 
         return services;
     }
