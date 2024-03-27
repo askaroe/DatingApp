a4bc546ecf98f42eeab8c5fec63dad75a7ae04cc
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-28 01:01:28 +0500

    fix: fixed date time issue by adding new auto mapper profile

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -23,6 +23,6 @@ public class AutoMapperProfiles : Profile
                 .FirstOrDefault(x => x.IsMain).Url));
         CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         CreateMap<DateTime?, DateTime?>().ConvertUsing(d => d.HasValue ? 
-            DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
+            DateTime.SpecifyKind(d.Value, DateTimeKind.Utc)  : null);
     }
 }
