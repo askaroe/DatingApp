3ba1e4c718a32c51fdd761a054b7554a7063c332
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-02 20:40:06 +0500

    refactor: optimized queries in message repository

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -84,9 +84,7 @@ public class MessageRepository : IMessageRepository
 
     public async Task<IEnumerable<MessageDTO>> GetMessageThread(string currentUserName, string recipientUserName)
     {
-        var messages = await _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+        var query = _context.Messages
             .Where(
                 m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
                 m.SenderUsername == recipientUserName || 
@@ -94,9 +92,9 @@ public class MessageRepository : IMessageRepository
                 m.SenderUsername == currentUserName 
             )
             .OrderBy(m => m.MessageSent)
-            .ToListAsync();
+            .AsQueryable();
 
-        var unreadMessages = messages.Where(m => m.DateRead == null 
+        var unreadMessages = query.Where(m => m.DateRead == null 
             && m.RecipientUsername == currentUserName).ToList();
 
         if(unreadMessages.Any())
@@ -108,7 +106,7 @@ public class MessageRepository : IMessageRepository
             
         }
 
-        return _mapper.Map<IEnumerable<MessageDTO>>(messages);
+        return await query.ProjectTo<MessageDTO>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
     public void RemoveConnection(Connection connection)
